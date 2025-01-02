165a4b0cf775480ee44e8cda1028e32f7a41ae33
Jonathan <j.sweet90@hotmail.com>
2025-01-01 19:18:22 -0600

    Create getHumanChoice function

--- foundations/rock-paper-scissors/javascript.js
+++ foundations/rock-paper-scissors/javascript.js
@@ -11,4 +11,17 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let choice = undefined; // Javascript doesn't like it when I try to make the variable in the while loop
+    while(true) {
+        choice = prompt("What do you choose?\n1 - Rock\n2 - Paper\n3 - Scissors");
+        choice = Number(choice);
+        if(!isNaN(choice) && choice > 0 && choice < 4) break;
+        prompt("You need a choose a number between 1-3!\nPress OK or Cancel.");
+    }
+    //console.log(choice);
+    return choice;
+}
+
 getComputerChoice();
+getHumanChoice();
