2c9235e3fa362a9e931acf3b3c858d99293bf583
Jonathan <j.sweet90@hotmail.com>
2025-01-01 19:04:31 -0600

    Create getComputerChoice function

--- foundations/rock-paper-scissors/javascript.js
+++ foundations/rock-paper-scissors/javascript.js
@@ -1,1 +1,14 @@
 console.log("Hello Odin Project!");
+
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 3) + 1);
+    //console.log(choice);
+
+    switch(choice) {
+        case 1: return "Rock";
+        case 2: return "Paper";
+        case 3: return "Scissors";
+    }
+}
+
+getComputerChoice();
