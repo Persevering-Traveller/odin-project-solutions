8f80535756c8ce1e3b854b225c6af0aff7d36c2f
Jonathan <j.sweet90@hotmail.com>
2025-01-08 16:29:38 -0600

    Remove unneeded getHumanChoice function

--- foundations/rock-paper-scissors/javascript.js
+++ foundations/rock-paper-scissors/javascript.js
@@ -13,19 +13,6 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = undefined; // Javascript doesn't like it when I try to make the variable in the while loop
-    while(true) {
-        choice = prompt("What do you choose?\n1 - Rock\n2 - Paper\n3 - Scissors");
-        if(choice.toLowerCase() == "rock" || choice.toLowerCase() == "paper" || choice.toLowerCase() == "scissors") break;
-        choice = Number(choice);
-        if(!isNaN(choice) && choice > 0 && choice < 4) break;
-        prompt("You need a choose a number between 1-3 or type the name of your choice!\nPress OK or Cancel.");
-    }
-    
-    return choice;
-}
-
 function playRound(humanChoice, computerChoice) {
     const resultsDiv = document.querySelector(".results");
     console.log(resultsDiv);
