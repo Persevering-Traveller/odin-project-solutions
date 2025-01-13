3c773403ceda9f9d8e8949d368cc05a3b8e98ab5
Jonathan <j.sweet90@hotmail.com>
2025-01-13 16:25:35 -0600

    Implement and run buildDrawingArea function

--- foundations/etch-a-sketch/javascript.js
+++ foundations/etch-a-sketch/javascript.js
@@ -1,3 +1,11 @@
 function buildDrawingArea() {
-    // TODO build a 16x16 grid of square divs
+    // Build a 16x16 grid of square divs
+    const body = document.querySelector(".container");
+    for(let i = 0; i < 16*16; i++) {
+        let square = document.createElement("div");
+        square.setAttribute("class", "square");
+        body.appendChild(square);
+    }
 }
+
+buildDrawingArea();
