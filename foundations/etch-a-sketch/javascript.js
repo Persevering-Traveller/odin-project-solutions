52d55b8b128233c91a54dd61430cb4572973ced5
Jonathan <j.sweet90@hotmail.com>
2025-01-13 17:02:52 -0600

    Add rebuildDrawingArea function

--- foundations/etch-a-sketch/javascript.js
+++ foundations/etch-a-sketch/javascript.js
@@ -8,4 +8,21 @@ function buildDrawingArea() {
     }
 }
 
+function rebuildDrawingArea(newPixelSize) {
+    const body = document.querySelector("body");
+    let container = document.querySelector(".container");
+    body.removeChild(container);
+
+    container = document.createElement("div");
+    container.setAttribute("class", "container");
+    body.appendChild(container);
+    for(let i = 0; i < newPixelSize * newPixelSize; i++) {
+        let square = document.createElement("div");
+        square.setAttribute("class", "square");
+        square.setAttribute("width", `${newPixeSize}px`);
+        square.setAttribute("height", `${newPixeSize}px`);
+        container.appendChild(square);
+    }
+}
+
 buildDrawingArea();
