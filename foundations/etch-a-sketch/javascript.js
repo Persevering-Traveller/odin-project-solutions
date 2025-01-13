fa3727870131279e4451cc206be64bdfdd23e92a
Jonathan <j.sweet90@hotmail.com>
2025-01-13 16:53:19 -0600

    Fix improper 'body' variable name to 'container'

--- foundations/etch-a-sketch/javascript.js
+++ foundations/etch-a-sketch/javascript.js
@@ -1,10 +1,10 @@
 function buildDrawingArea() {
     // Build a 16x16 grid of square divs
-    const body = document.querySelector(".container");
+    const container = document.querySelector(".container");
     for(let i = 0; i < 16*16; i++) {
         let square = document.createElement("div");
         square.setAttribute("class", "square");
-        body.appendChild(square);
+        container.appendChild(square);
     }
 }
 
