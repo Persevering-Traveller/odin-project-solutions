ea12743b4b5f012551662f95d73228c76c14a387
Jonathan <j.sweet90@hotmail.com>
2025-01-21 17:54:39 -0600

    Add reference to calculator screen and have number buttons change it when pressed

--- foundations/calculator/javascript.js
+++ foundations/calculator/javascript.js
@@ -12,6 +12,7 @@ removeLastEntry - Will remove whatever numeral or dot(.) the user enters
 */
 
 // Javascript has no enums and that's a shame...
+const NO_OP = -1
 const OP_ADD = 0;
 const OP_SUB = 1;
 const OP_MUL = 2;
@@ -20,17 +21,36 @@ const OP_DIV = 3;
 let numberBank1 = 0;
 let numberBank2 = 0;
 let operation = -1;
+let operationString = " ";
+
+const calculatorScreen = document.querySelector(".screen");
 
 // Add onclick button listeners
+// Number buttons
 const buttonCollection = document.querySelectorAll(".number");
 buttonCollection.forEach((button, i) => {
     button.addEventListener("click", () => {
-        if (i === 9) return;
-        (operation === -1) ? numberBank1 = (numberBank1 * 10) + (i + 1) : numberBank2 = (numberBank2 * 10) + (i + 1);
+        if(i === 9) return;
+        if(operation === NO_OP){ 
+            numberBank1 = (numberBank1 * 10) + (i + 1);
+            calculatorScreen.textContent = numberBank1;
+         }
+        else {
+            numberBank2 = (numberBank2 * 10) + (i + 1);
+            calculatorScreen.textContent = numberBank1 + operationString + numberBank2;
+        }
     });
 });
 
 // since the zero button is the last in the collection and operates differently, directly set it
 buttonCollection[buttonCollection.length - 1].addEventListener("click", () => {
-    (operation === -1) ? numberBank1 *= 10 : numberBank2 *= 10;
+    if(operation === -1) {
+        numberBank1 *= 10;
+        calculatorScreen.textContent = numberBank1;
+    }
+    else {
+        numberBank2 *= 10;
+        calculatorScreen.textContent = numberBank1 + operationString + numberBank2;
+    }
+        
 });
