391080087a59438cc12d724371ea135a37c31220
Jonathan <j.sweet90@hotmail.com>
2025-03-05 14:16:18 -0600

    Add showLibrary and helper createNewItem functions

--- node-path/library/scripts.js
+++ node-path/library/scripts.js
@@ -11,3 +11,24 @@ function Book(name, author, pages, read) {
 function addBookToLibrary(name, author, pages, read) {
     myLibrary.push(new Book(name, author, pages, read));
 }
+
+function showLibrary() {
+    const libraryTable = document.querySelector(".library-table");
+
+    for(let i = 0; i < myLibrary.length; i++) {
+        let newBookRow = document.createElement("tr");
+
+        createNewItem(myLibrary[i].name, newBookRow);
+        createNewItem(myLibrary[i].author, newBookRow);
+        createNewItem(myLibrary[i].pages, newBookRow);
+        createNewItem((myLibrary[i].read) ? "Yes" : "No", newBookRow);
+
+        libraryTable.appendChild(newBookRow);
+    }
+}
+
+function createNewItem(contents, tableRow) {
+    newItem = document.createElement("td");
+    newItem.innerText = contents;
+    tableRow.appendChild(newItem);
+}
