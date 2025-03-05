2c74bb2adcbb1622234c2612828d4116da568888
Jonathan <j.sweet90@hotmail.com>
2025-03-05 14:02:06 -0600

    Add properties to Book object, and functionality to addBookToLibrary

--- node-path/library/scripts.js
+++ node-path/library/scripts.js
@@ -1,9 +1,13 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(name, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(name, author, pages, read) {
+    myLibrary.push(new Book(name, author, pages, read));
 }
