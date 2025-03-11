67922e99d4a83d76ee33d16fd98aefe112e8a2ba
Jonathan <j.sweet90@hotmail.com>
2025-03-11 15:40:34 -0500

    Add initial gameManager object functionality

--- node-path/tic-tac-toe/scripts.js
+++ node-path/tic-tac-toe/scripts.js
@@ -35,3 +35,59 @@ function createPlayer(sign) {
 
     return { getSign, assignNewSign, getScore, givePoint };
 }
+
+const gameManager = (function () {
+    let player, cpu;
+
+    const startGame = () => {
+        gameBoard.createGameBoard();
+        player = createPlayer("X");
+        cpu = createPlayer("O");
+    }
+
+    const checkWinner = function () {
+        const currentBoard = gameBoard.getBoard();
+        // Check for all possible horizontal wins, all vertical wins, and the cross wins
+        /* 0 | 1 | 2
+         * 3 | 4 | 5
+         * 6 | 7 | 8 */
+        let winningSign = "";
+        // Horizontal Checks
+        for(let i = 0; i < 9; i += 3) {
+            let sign = currentBoard[i];
+            if(sign === " ")
+                continue;
+            if(sign === currentBoard[i+1] && sign === currentBoard[i+2]) {
+                winningSign = sign;
+                break;
+            }
+        }
+        // Vertical Checks
+        for(let i = 0; i < 3; i++) {
+            let sign = currentBoard[i];
+            if(sign === " ")
+                continue;
+            if(sign === currentBoard[i+3] && sign === currentBoard[i+6]) {
+                winningSign = sign;
+                break;
+            }
+        }
+        // Cross Checks
+        sign = currentBoard[0]; // The \ cross win
+        if(sign !== " " && sign === currentBoard[4] && sign === currentBoard[8])
+            winningSign = sign;
+        sign = currentBoard[3] // The / cross win
+        if(sign !== " " && sign === currentBoard[4] && sign === currentBoard[6])
+            winningSign = sign;
+
+        // Declare winner
+        if(winningSign !== "") {
+            if(winningSign === player.getSign())
+                console.log("You win!!");
+            else if (winningSign === cpu.getSign())
+                console.log("The CPU wins this round!");
+        }
+    }
+
+    return { startGame, checkWinner };
+})();
